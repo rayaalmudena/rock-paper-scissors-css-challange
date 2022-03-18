1c1d7a1ed6f42aca2608406323534c51669408a8
rayaalmudena <raya.almudena@gmail.com>
2022-03-18 13:05:14 +0100

    funciona pero no esta bonito

---
+++ rock-paper-scissors-master/play.js
@@ -0,0 +1,75 @@
+let userPick="";
+const serverPickArray=["rock","paper","scissors"];
+
+document.querySelector("#rock-option").addEventListener("click", saveElection);
+document.querySelector("#paper-option").addEventListener("click", saveElection);
+document.querySelector("#scissors-option").addEventListener("click", saveElection);
+
+
+function saveElection(event) {
+    userPick=event.currentTarget.getAttribute("data-option");   
+    document.querySelector(".choose-option").style.display="none"; 
+    document.querySelector("#end-of-game").style.display="flex";
+    document.querySelector("#your-pick").src="./images/icon-"+userPick+".svg";      
+}
+
+
+document.querySelector(".house-pick").addEventListener("click", showelectionhouse);
+
+function showelectionhouse(){
+    var random = serverPickArray[Math.floor(Math.random()*serverPickArray.length)];
+    document.querySelector("#random-bot-pick").src="./images/icon-"+random+".svg";
+    document.querySelector("#random-bot-pick").style.display="block";
+    winlose(random);    
+    document.querySelector(".house-pick").removeEventListener("click", showelectionhouse);
+
+}
+
+function winlose(random){
+    if(userPick==random){
+        console.log("Empate");
+        document.querySelector(".text-draw").style.display="flex";
+    }
+    else if((userPick=="paper" && random=="scissors")||(userPick=="scissors" && random=="rock")||(userPick=="rock" && random =="paper")){
+        console.log("Has perdido");
+        changeScore(false);  
+        document.querySelector(".text-you-lose").style.display="flex";
+    }
+    else{
+        console.log("Has ganado")
+        changeScore(true);      
+        document.querySelector(".text-you-win").style.display="flex";
+    }
+    document.querySelector(".result-end-game").style.display="flex";
+    
+}
+
+
+
+function changeScore(e){
+    let score = +document.querySelector("#score").innerHTML;
+    if(e==true){        
+        document.querySelector("#score").innerHTML=score+1;  
+        console.log(score);
+        }
+    else{
+        document.querySelector("#score").innerHTML=score-1;  
+        console.log(score);
+    }
+}
+
+document.querySelector(".btn-play-again").addEventListener("click",playAgain);
+
+function playAgain() {
+
+    document.querySelector(".choose-option").style.display="grid"; 
+    document.querySelector("#end-of-game").style.display="none";
+
+    document.querySelector(".text-draw").style.display="none";
+    document.querySelector(".text-you-lose").style.display="none";
+    document.querySelector(".result-end-game").style.display="none";
+    document.querySelector(".text-you-win").style.display="none";
+    
+    document.querySelector("#random-bot-pick").style.display="none";
+    document.querySelector(".house-pick").addEventListener("click", showelectionhouse);
+}
